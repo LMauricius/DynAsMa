b303c665a0251bd1acc08109fd1c3825ce969110
Mauricio Šmit <mauricio.smit@ses-imagotag.com>
2023-11-19 14:19:35 +0100

    Added allocator concept and others

--- include/dynasma/util/helpful_concepts.hpp
+++ include/dynasma/util/helpful_concepts.hpp
@@ -35,6 +35,56 @@ concept KeyUsable = Hashable<T> || KeyComparable<T>;
 template <typename T>
 concept ConstQualified = std::same_as<T, std::remove_const_t<T>>;
 
+/**
+ * @brief Whether it is volatile qualified
+ */
+template <typename T>
+concept VolatileQualified = std::same_as<T, std::remove_volatile_t<T>>;
+
+/**
+ * @brief Whether it is cv qualified
+ */
+template <typename T>
+concept CVQualified = ConstQualified<T> || VolatileQualified<T>;
+template <typename T>
+concept NotCVQualified = !CVQualified<T>;
+
+/**
+ * @brief Allocator
+ */
+template <class A, typename T = A::value_type,
+          typename pointer = std::allocator_traits<A>::pointer,
+          typename const_pointer = std::allocator_traits<A>::const_pointer,
+          typename void_pointer = std::allocator_traits<A>::void_pointer,
+          typename const_void_pointer =
+              std::allocator_traits<A>::const_void_pointer,
+          typename size_type = std::allocator_traits<A>::size_type,
+          typename difference_type = std::allocator_traits<A>::difference_type>
+concept AllocatorLike =
+    requires(A a, pointer p, const_pointer cp, void_pointer vp,
+             const_void_pointer cvp, decltype(*p) r, size_type n) {
+        typename T;
+
+        // *p
+        { *p } -> std::same_as<T &>;
+
+        // *cp
+        { *cp } -> std::same_as<const T &>;
+
+        // static_cast<Alloc::pointer>(vp)
+        { static_cast<pointer>(vp) } -> std::same_as<pointer>;
+
+        // static_cast<Alloc::const_pointer>(cvp)
+        { static_cast<const_pointer>(cvp) } -> std::same_as<const_pointer>;
+
+        // std::pointer_traits<Alloc::pointer>::pointer_to(r)
+        { std::pointer_traits<pointer>::pointer_to(r) };
+
+        { a.allocate(n) } -> std::same_as<pointer>;
+
+        { a.deallocate(p, n) } -> std::same_as<void>;
+    };
+
 } // namespace dynasma
 
 #endif // INCLUDED_DYNASMA_HELPFUL_CONCEPTS_H
