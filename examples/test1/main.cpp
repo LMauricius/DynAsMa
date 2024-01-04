20e2ca5d6ed576abad24b7677f10bd0ce89b99aa
LMauricius <mauriciosmit100@gmail.com>
2024-01-04 01:21:24 +0100

    A more robust basic test

--- examples/test1/main.cpp
+++ examples/test1/main.cpp
@@ -7,9 +7,9 @@
 class TestAsset {
   public:
     TestAsset(std::string name) {
-        std::cout << "TestAsset " << name << " constructed!" << std::endl;
+        std::cout << "--- TestAsset " << name << " constructed!" << std::endl;
     }
-    ~TestAsset() { std::cout << "TestAsset destructed!" << std::endl; }
+    ~TestAsset() { std::cout << "--- TestAsset destructed!" << std::endl; }
 
     std::size_t memory_cost() const { return sizeof(TestAsset); }
 };
@@ -19,47 +19,52 @@ struct TestSeed {
     std::variant<std::string> kernel;
 
     std::size_t load_cost() const { return 1; }
+    std::string name() const { return std::get<std::string>(kernel); }
 };
 
 // A function taking template template parameter Manager that tests it
 template <template <typename, typename> typename Manager> void testManager() {
     Manager<TestSeed, std::allocator<TestAsset>> manager;
 
+    const TestSeed seed1{"<My asset 1>"};
+    const TestSeed seed2{"<My asset 2>"};
+
+    dynasma::WeakPtr<TestAsset> weakPtr1;
+    dynasma::WeakPtr<TestAsset> weakPtr2;
+
     std::cout << "Entering block...\n";
     {
-        std::cout << "Entered block!\n";
+        std::cout << "    Entered block!\n";
 
-        std::cout << "Registering asset 1...\n";
-        dynasma::WeakPtr<TestAsset> weakPtr =
-            manager.register_asset({"<My asset 1>"});
-        std::cout << "Asset registered!\n";
+        std::cout << "    Registering asset 1...\n";
+        weakPtr1 = manager.register_asset(seed1);
+        std::cout << "    Asset registered!\n";
 
-        std::cout << "Taking strong reference...\n";
-        dynasma::StrongPtr<TestAsset> strongPtr = weakPtr;
-        std::cout << "Strong reference taken!\n";
+        std::cout << "    Taking strong reference...\n";
+        dynasma::StrongPtr<TestAsset> strongPtr = weakPtr1;
+        std::cout << "    Strong reference taken!\n";
 
-        std::cout << "Asset pointer: " << &*strongPtr << "\n";
+        std::cout << "    Asset pointer: " << &*strongPtr << "\n";
 
-        std::cout << "Exiting block...\n";
+        std::cout << "    Exiting block...\n";
     }
     std::cout << "Exited block!\n";
 
     std::cout << "Entering block...\n";
     {
-        std::cout << "Entered block!\n";
+        std::cout << "    Entered block!\n";
 
-        std::cout << "Registering asset 2...\n";
-        dynasma::WeakPtr<TestAsset> weakPtr =
-            manager.register_asset({"<My asset 2>"});
-        std::cout << "Asset registered!\n";
+        std::cout << "    Registering asset 2...\n";
+        weakPtr2 = manager.register_asset(seed2);
+        std::cout << "    Asset registered!\n";
 
-        std::cout << "Taking strong reference...\n";
-        dynasma::StrongPtr<TestAsset> strongPtr = weakPtr;
-        std::cout << "Strong reference taken!\n";
+        std::cout << "    Taking strong reference...\n";
+        dynasma::StrongPtr<TestAsset> strongPtr = weakPtr2;
+        std::cout << "    Strong reference taken!\n";
 
-        std::cout << "Asset pointer: " << &*strongPtr << "\n";
+        std::cout << "    Asset pointer: " << &*strongPtr << "\n";
 
-        std::cout << "Exiting block...\n";
+        std::cout << "    Exiting block...\n";
     }
     std::cout << "Exited block!\n";
 
@@ -77,10 +82,10 @@ template <template <typename, typename> typename Manager> void testManager() {
 }
 
 int main() {
-    std::cout << "TESTING NaiveManager" << std::endl;
+    std::cout << "==== TESTING NaiveManager ==== " << std::endl;
     testManager<dynasma::NaiveManager>();
 
-    std::cout << "TESTING BasicManager" << std::endl;
+    std::cout << "==== TESTING BasicManager ==== " << std::endl;
     testManager<dynasma::BasicManager>();
 
     return 0;
